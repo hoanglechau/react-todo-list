6cd448e0aeae17b94e8bc10fea74328601fab115
hoanglechau <hoangchau612@gmail.com>
2023-03-01 18:00:37 +0700

    Create TaskList.js

---
+++ src/components/TaskList.js
@@ -0,0 +1,39 @@
+import React from "react";
+import TaskItem from "./TaskItem";
+
+function TaskList({
+  tasks,
+  hideCompleted,
+  setTaskStatus,
+  removeTask,
+  handleHideCompleted,
+}) {
+  return (
+    <>
+      <ul className="task-list">
+        {tasks
+          .filter((task) => (hideCompleted ? task.status !== true : true))
+          .map((task) => (
+            <TaskItem
+              task={task}
+              setTaskStatus={setTaskStatus}
+              removeTask={removeTask}
+            />
+          ))}
+      </ul>
+      <div className="filter-wrapper">
+        <label htmlFor="filter" className="filter-label">
+          Hide completed tasks
+        </label>
+        <input
+          type="checkbox"
+          id="filter"
+          checked={hideCompleted}
+          onChange={handleHideCompleted}
+        />
+      </div>
+    </>
+  );
+}
+
+export default TaskList;
